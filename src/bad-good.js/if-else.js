11ad0b0140a6603ab2f1163ebf2aa554b3322012
Sanghak,Lee <code.ryan.lee@gmail.com>
2017-08-25 17:12:58 +0900

    if-else 예제 추가

---
+++ src/bad-good.js/if-else.js
@@ -0,0 +1,54 @@
+const ES5 = {
+    "if-else" : (name, age, email) => {
+        if ( name && name.length > 3 ) {
+            // something
+            // ...
+            if ( age < 30 ) {
+                // something
+                // ...
+                if ( /이메일정규식/.test(email) ) {
+                    // something
+                    // ...
+                    let code = true
+                    let msg = 'all ok!'
+                } else {
+                    let code = false
+                    let msg = 'email validation error'
+                }
+            } else {
+                let code = false
+                let msg = 'age validation error'
+            }
+        } else {
+            let code = false
+            let msg = 'name validation error'
+        }
+
+        return {code, msg}
+    }
+}
+
+const ES6 = {
+    
+    "if-else" : (name, age, email) => {
+        try {
+            if ( !name || name.length < 4 ) {
+                throw 'name validation error'
+            }
+            if ( age >= 30 ) {
+                throw 'age validation error'
+            }
+            if ( !/이메일정규식/.test(email) ) {
+                throw 'email validation error'
+            }
+
+            // something
+            // ...
+            return {code: true, msg: 'all ok!'}
+        } catch(e) {
+            return {code: false, msg: e}
+        }
+    }
+}
+
+
